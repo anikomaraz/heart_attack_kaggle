f4973b0693effa3c2ba31feb664e0ea01f4de5b2
Aniko Maraz <aniko.maraz@gmail.com>
2024-07-08 20:20:56 +0200

    sidebar-pointing text added2

--- app_heart_attack.py
+++ app_heart_attack.py
@@ -22,7 +22,7 @@ st.markdown("""
 This is a unique opportunity to fake-predict your heart risk based on fake data provided by Kaggle.
 Please enter your fake data - you are welcome to modify the default values.
 <br>
-&larr; See the sidebar for technical details on the project.
+&#8592; See the sidebar for technical details on the project.
 """, unsafe_allow_html=True)
 
 
