72130df8ed5a8a62510dc48df0ae3eadfd7ed487
Aniko Maraz <aniko.maraz@gmail.com>
2024-07-08 17:36:59 +0200

    coloring2

--- app_heart_attack.py
+++ app_heart_attack.py
@@ -236,22 +236,13 @@ st.sidebar.markdown(
     unsafe_allow_html=True
 )
 
-# Set custom CSS to style the sidebar background, text, and titles
+# Set custom CSS to style the sidebar background
 st.sidebar.markdown(
     """
     <style>
     .sidebar .sidebar-content {
         background-color: #D3D3D3;  /* Medium grey background */
         padding: 10px;
-    }
-    .sidebar .sidebar-content h2 {
-        color: #228B22;  /* Green color for main titles */
-    }
-    .sidebar .sidebar-content h3 {
-        color: #228B22;  /* Green color for numbered titles */
-    }
-    .sidebar .sidebar-content p {
-        font-size: 14px; /* Adjust paragraph font size if needed */
     }
     </style>
     """,
@@ -261,28 +252,38 @@ st.sidebar.markdown(
 # Add the content to the sidebar
 st.sidebar.markdown(
     """
-    ## Project Overview: Heart Attack Risk Analysis
+    <h2 style="color:#228B22;">Project Overview: Heart Attack Risk Analysis</h2>
 
-    This project was developed for the [Heart Attack Risk Analysis](https://www.kaggle.com/competitions/heart-attack-risk-analysis/overview) competition on Kaggle, with predictions submitted to an ongoing challenge.
+    This project was developed for the <a href="https://www.kaggle.com/competitions/heart-attack-risk-analysis/overview">Heart Attack Risk Analysis</a> competition on Kaggle, with predictions submitted to an ongoing challenge.
 
-    ### Key Project Phases
+    <h3 style="color:#228B22;">Key Project Phases</h3>
 
-    1. **_Exploratory Data Analysis and Preprocessing:_**
-       - Explored the dataset thoroughly to uncover patterns and distributions ([see notebook](https://github.com/anikomaraz/heart_attack_kaggle/blob/main/notebooks/heart_attack_v3_clean_KaggleV1.ipynb)).
-       - Implemented extensive preprocessing including feature engineering, encoding, normalization, and balancing to prepare the data for modeling.
+    <ol>
+        <li><b><i style="color:#228B22;">Exploratory Data Analysis and Preprocessing:</i></b>
+            <ul>
+                <li>Explored the dataset thoroughly to uncover patterns and distributions (<a href="https://github.com/anikomaraz/heart_attack_kaggle/blob/main/notebooks/heart_attack_v3_clean_KaggleV1.ipynb">see notebook</a>).</li>
+                <li>Implemented extensive preprocessing including feature engineering, encoding, normalization, and balancing to prepare the data for modeling.</li>
+            </ul>
+        </li>
+        <li><b><i style="color:#228B22;">Model Development and Optimization:</i></b>
+            <ul>
+                <li>Trained and evaluated six machine learning models including Logistic Regression, XGBoost, SVM, Decision Tree, Random Forest, and Gradient Boosting.</li>
+                <li>Hyperparameter-tuned the XGBoost and SVM models to improve performance metrics, selected <b><i style="color:#228B22;">XGBoost</i></b> for best performance.</li>
+                <li>Given the low number of positive cases on the test set, employed <b><i style="color:#228B22;">probability estimation</i></b> and found an optimal threshold to increase sensitivity.</li>
+            </ul>
+        </li>
+        <li><b><i style="color:#228B22;">Focus on Precision:</i></b>
+            <ul>
+                <li>Although the competition primarily focused on accuracy, I prioritized <b><i style="color:#228B22;">precision</i></b> in my model to enhance its capability in correctly identifying positive cases. This strategic adjustment resulted in a higher confidence level for risk assessment.</li>
+                <li>Documentation can be found in <a href="https://github.com/anikomaraz/heart_attack_kaggle/blob/main/notebooks/heart_attack_v5_probability_xgboost_KaggleV2.ipynb">this notebook</a></li>
+            </ul>
+        </li>
+    </ol>
 
-    2. **_Model Development and Optimization:_**
-       - Trained and evaluated six machine learning models including Logistic Regression, XGBoost, SVM, Decision Tree, Random Forest, and Gradient Boosting.
-       - Hyperparameter-tuned the XGBoost and SVM models to improve performance metrics, selected **_XGBoost_** for best performance.
-       - Given the low number of positive cases on the test set, employed **_probability estimation_** and found an optimal threshold to increase sensitivity.
+    <h3 style="color:#228B22;">Model Deployment and GitHub Repository</h3>
 
-    3. **_Focus on Precision:_**
-       - Although the competition primarily focused on accuracy, I prioritized **_precision_** in my model to enhance its capability in correctly identifying positive cases. This strategic adjustment resulted in a higher confidence level for risk assessment.
-
-    ### Model Deployment and GitHub Repository
-
-    - The project evolved through multiple versions, culminating in a deployed application hosted on **_Google Cloud Platform_** with a Streamlit frontend.
-    - For more detailed insights, including comprehensive project details, versioning, and visualizations, visit my [GitHub Repository](https://github.com/anikomaraz/heart_attack_kaggle#).
+    - The project evolved through multiple versions, culminating in a deployed application hosted on <b><i style="color:#228B22;">Google Cloud Platform</i></b> with a Streamlit frontend.
+    - For more detailed insights, including comprehensive project details, versioning, and visualizations, visit my <a href="https://github.com/anikomaraz/heart_attack_kaggle#">GitHub Repository</a>.
     """,
     unsafe_allow_html=True
 )
