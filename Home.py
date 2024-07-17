d184681abaad4755ba9942f3c60e74631176f383
Aniko Maraz <aniko.maraz@gmail.com>
2024-07-17 14:54:08 +0200

    text adjustments

--- Home.py
+++ Home.py
@@ -17,9 +17,13 @@ st.markdown("""
     h2, h3 {
         color: #228B22;  /* Green color for headings */
     }
+    h4 {
+        color: #ff2b2b;  /* Red color for h4 */
+    }
     </style>
     <div class="centered-text">
         <h1>Welcome to my heart attack risk prediction app. ❤️</h1> 
+        <h4>This is a unique opportunity to fake-predict your heart attack risk. Please note that this model was trained on fake data, so don't take the prediction too seriously. Having said that it should generally point you to the right direction, so if you want to minimise your chance of a heart attack, then live a healthy life!️</h4> 
         <p></p>
         <p></p>
     </div>
@@ -31,28 +35,29 @@ st.markdown(
 
     <h3>Quick Summary</h3>
 
-    This project was inspired by the [Heart Attack Risk Analysis](https://www.kaggle.com/competitions/heart-attack-risk-analysis/overview) competition on Kaggle, with predictions submitted to an ongoing challenge. 
-    The task was to predict heart attack risk (low/high) given 25 features of lifestyle and biometrics. Focusing on precision, I built and deployed my own model which is currently running in production on this website. I will provide a quick summary of the app's features the following phases, steps and highlights. Please look for the [Technical Summary](https://fake-heart-attack.streamlit.app/Technical_summary) for details on preprocessing, modelling and evaluation.
+This project was inspired by the [Heart Attack Risk Analysis](https://www.kaggle.com/competitions/heart-attack-risk-analysis/overview) competition on Kaggle, with predictions submitted to an ongoing challenge. The task was to predict heart attack risk (low/high) given 25 features of lifestyle and biometrics. Focusing on precision, I developed and deployed my own model currently in production on this website. Below is a quick summary of the app's key phases, steps, and highlights. For detailed information on preprocessing, modeling, and evaluation, refer to the [Technical Summary](https://fake-heart-attack.streamlit.app/Technical_summary).
 
-    1. **Exploratory Data Analysis and Preprocessing:**
-       - Explored the dataset thoroughly to uncover patterns and distributions ([see notebook](https://nbviewer.org/github/anikomaraz/heart_attack_kaggle/blob/main/notebooks/heart_attack_v3_clean_KaggleV1.ipynb)).
-       - Implemented extensive preprocessing including feature engineering, encoding, normalization, and balancing to prepare the data for modeling.
+1. **Exploratory Data Analysis and Preprocessing:**
+   - I thorouly explored the dataset to uncover patterns and distributions ([notebook link](https://nbviewer.org/github/anikomaraz/heart_attack_kaggle/blob/main/notebooks/heart_attack_v3_clean_KaggleV1.ipynb)).
+   - Extensive preprocessing included feature engineering, encoding, normalization, and balancing to prepare data for modeling.
 
-    2. **Model Development and Optimization:**
-       - Trained and evaluated seven machine learning models including Logistic Regression, XGBoost, SVM, Decision Tree, Random Forest, Gradient Boosting, and Neural Networks.
-       - Hyperparameter-tuned the XGBoost, SVM, and the Neural Network models to improve performance metrics.
+2. **Model Development and Optimization:**
+   - Trained and evaluated seven machine learning models: Logistic Regression, XGBoost, SVM, Decision Tree, Random Forest, Gradient Boosting, and Neural Networks.
+   - Hyperparameter-tuned XGBoost, SVM, and Neural Network models to enhance performance metrics.
 
-    3. **Focus on Precision:**
-       - However, I argue that the cost of missing truly high-risk cases is high, therefore **precision** is a more suitable metric of model performance.
-       - The model with the highest precision was **XGBoost**.
-       - To further enhance detection and to account for the lack of positive predictions on the test set, I employed **probability estimation** and found an optimal threshold to increase sensitivity.
-       - This strategic adjustment resulted in a higher sensitivity for risk assessment, therefore a better detection for high-risk heart attack cases.
-       - Documentation can be found in [this notebook](https://nbviewer.org/github/anikomaraz/heart_attack_kaggle/blob/main/notebooks/heart_attack_v5_probability_xgboost_KaggleV2.ipynb).
+3. **Focus on Precision:**
+   - I advocate that precision is a better metric than accuracy in this case. Detecting high risk cases is cruical in this case, which should be the focus of this model instead of classification accuracy as suggested by the competition.
+   - Therefore I optimised model performance for precision and identified **XGBoost** as the highest performing model.
+   - Given the low number of predicted positive cases on the test set, I utilized **probability estimation** to adjust sensitivity thresholds, resulting in improved high-risk heart attack detection.
+   - Refer to [this notebook](https://nbviewer.org/github/anikomaraz/heart_attack_kaggle/blob/main/notebooks/heart_attack_v5_probability_xgboost_KaggleV2.ipynb) for detailed documentation.
 
-    ### Model Deployment
-    - The project evolved through multiple versions, culminating in a deployed application hosted on **Google Cloud Platform** with a Streamlit frontend.
-    - For more detailed insights, including comprehensive project details, versioning, and visualizations, go to [Technical Summary](https://fake-heart-attack.streamlit.app/Technical_summary) on this website, or visit my [GitHub Repository](https://github.com/anikomaraz/heart_attack_kaggle#).
-    """,
+4. **Model Evaluation**
+    - If the model identifies a positive case, it is correct **43% of the time** (=precision).
+
+### Model Deployment
+- The project progressed through multiple iterations, culminating in deployment on **Google Cloud Platform** with a Streamlit frontend.
+- For comprehensive project details, versioning, and visualizations, visit the [Technical Summary](https://fake-heart-attack.streamlit.app/Technical_summary) on this website.
+
     unsafe_allow_html=True
 )
 
@@ -83,7 +88,7 @@ git_icon_url = "https://github.githubassets.com/assets/GitHub-Mark-ea2971cee799.
 # Add GitHub with href format
 st.markdown(f"""
     <br>
-    Visit the Project's <a href="https://github.com/anikomaraz/heart_attack_kaggle" target="_blank">GitHub Repository</a> <img src="{git_icon_url}" width="20">
+    For more details please visit the project's <a href="https://github.com/anikomaraz/heart_attack_kaggle" target="_blank">GitHub Repository</a> <img src="{git_icon_url}" width="20">
     <br>
     """, unsafe_allow_html=True)
 
