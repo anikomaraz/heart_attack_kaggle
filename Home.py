654192b68493f8e8c23c701ad8a0982bf2dfb04e
Aniko Maraz <aniko.maraz@gmail.com>
2024-07-17 14:20:24 +0200

    rendering issue

--- Home.py
+++ Home.py
@@ -14,6 +14,9 @@ st.markdown("""
         font-size: 45px;
         font-weight: bold;
     }
+    h2, h3 {
+        color: #228B22;  /* Green color for headings */
+    }
     </style>
     <div class="centered-text">
         <h1>Welcome to my heart attack risk prediction app. ❤️</h1> <br> <br>
@@ -25,41 +28,30 @@ st.markdown("""
 
 st.markdown(
     """
-    <h2 style="color:#228B22;"></h2>
+    <h2></h2>
 
-    <h3 style="color:#228B22;">Quick Summary</h3>
+    <h3>Quick Summary</h3>
 
-    This project was inspired by the <a href="https://www.kaggle.com/competitions/heart-attack-risk-analysis/overview">Heart Attack Risk Analysis</a> competition on Kaggle, with predictions submitted to an ongoing challenge. 
+    This project was inspired by the [Heart Attack Risk Analysis](https://www.kaggle.com/competitions/heart-attack-risk-analysis/overview) competition on Kaggle, with predictions submitted to an ongoing challenge. 
     The task was to predict heart attack risk (low/high) given 25 features of lifestyle and biometrics. Focusing on precision, I built and deployed my own model which is currently running in production on this website. I will provide a quick summary of the app's features the following phases, steps and highlights. Please look for the [Technical Summary](https://fake-heart-attack.streamlit.app/Technical_summary) for details on preprocessing, modelling and evaluation.
 
-    <ol>
-        <li><b><i style="color:#228B22;">Exploratory Data Analysis and Preprocessing:</i></b>
-            <ul>
-                <li>Explored the dataset thoroughly to uncover patterns and distributions (<a href="https://nbviewer.org/github/anikomaraz/heart_attack_kaggle/blob/main/notebooks/heart_attack_v3_clean_KaggleV1.ipynb">see notebook</a>).</li>
-                <li>Implemented extensive preprocessing including feature engineering, encoding, normalization, and balancing to prepare the data for modeling.</li>
-            </ul>
-        </li>
-        <li><b><i style="color:#228B22;">Model Development and Optimization:</i></b>
-            <ul>
-                <li>Trained and evaluated seven machine learning models including Logistic Regression, XGBoost, SVM, Decision Tree, Random Forest, Gradient Boosting and Neural Networks.</li>
-                <li>Hyperparameter-tuned the XGBoost, SVM and the Neural Network models to improve performance metrics.</li>
+    1. **Exploratory Data Analysis and Preprocessing:**
+       - Explored the dataset thoroughly to uncover patterns and distributions ([see notebook](https://nbviewer.org/github/anikomaraz/heart_attack_kaggle/blob/main/notebooks/heart_attack_v3_clean_KaggleV1.ipynb)).
+       - Implemented extensive preprocessing including feature engineering, encoding, normalization, and balancing to prepare the data for modeling.
 
-            </ul>
-        </li>
-        <li><b><i style="color:#228B22;">Focus on Precision:</i></b>
-            <ul>
-                <li>However, I argue that the cost of missing truly high risk cases is high, therefore <b>precision</b> is a more suitable metric of model performance.</li>
-                <li>The model with the highest precision was <b>XGBoost</b>.</li>
-                <li>To further enhance detection and to account for the lack of positive predictions on the test set, I employed <b>probability estimation</b> and found an optimal threshold to increase sensitivity.</li>
-                <li>This strategic adjustment resulted in a higher sensitivity for risk assessment, therefore a better detection for high risk heart attack cases.</li>
-                <li>Documentation can be found in <a href="https://nbviewer.org/github/anikomaraz/heart_attack_kaggle/blob/main/notebooks/heart_attack_v5_probability_xgboost_KaggleV2.ipynb">this notebook.</a></li>
-            </ul>
-        </li>
-    </ol>
+    2. **Model Development and Optimization:**
+       - Trained and evaluated seven machine learning models including Logistic Regression, XGBoost, SVM, Decision Tree, Random Forest, Gradient Boosting, and Neural Networks.
+       - Hyperparameter-tuned the XGBoost, SVM, and the Neural Network models to improve performance metrics.
 
-    <h3 style="color:#228B22;">Model Deployment</h3>
+    3. **Focus on Precision:**
+       - However, I argue that the cost of missing truly high-risk cases is high, therefore **precision** is a more suitable metric of model performance.
+       - The model with the highest precision was **XGBoost**.
+       - To further enhance detection and to account for the lack of positive predictions on the test set, I employed **probability estimation** and found an optimal threshold to increase sensitivity.
+       - This strategic adjustment resulted in a higher sensitivity for risk assessment, therefore a better detection for high-risk heart attack cases.
+       - Documentation can be found in [this notebook](https://nbviewer.org/github/anikomaraz/heart_attack_kaggle/blob/main/notebooks/heart_attack_v5_probability_xgboost_KaggleV2.ipynb).
 
-    - The project evolved through multiple versions, culminating in a deployed application hosted on <b>Google Cloud Platform</b> with a Streamlit frontend.
+    ### Model Deployment
+    - The project evolved through multiple versions, culminating in a deployed application hosted on **Google Cloud Platform** with a Streamlit frontend.
     - For more detailed insights, including comprehensive project details, versioning, and visualizations, go to [Technical Summary](https://fake-heart-attack.streamlit.app/Technical_summary) on this website, or visit my [GitHub Repository](https://github.com/anikomaraz/heart_attack_kaggle#).
     """,
     unsafe_allow_html=True
@@ -68,9 +60,12 @@ st.markdown(
 st.markdown("""
     <br>
     <br>
-    <h2 style="color:#228B22;">Choose an Action:</h2>
+    <h2>Choose an Action:</h2>
     <br>
     """, unsafe_allow_html=True)
+
+# GitHub logo URL
+github_logo_url = "https://github.githubassets.com/images/modules/logos_page/GitHub-Mark.png"
 
 # Define the buttons with HTML formatting
 buttons = [
@@ -81,4 +76,4 @@ buttons = [
 
 # Display buttons
 for button in buttons:
-    st.markdown(f'<a href="{button["url"]}"><button>{button["label"]}</button></a>', unsafe_allow_html=True)
+    st.button(button["label"], on_click=button["url"], unsafe_allow_html=True)
