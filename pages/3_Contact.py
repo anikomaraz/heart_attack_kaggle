fa880e8417790ad7d722c1a78364d7509c3c98af
Aniko Maraz <aniko.maraz@gmail.com>
2024-07-17 17:47:03 +0200

    minor typos

--- pages/3_Contact.py
+++ pages/3_Contact.py
@@ -9,7 +9,7 @@ st.set_page_config(
 
 st.markdown(f"""
    ✉️ aniko.maraz  [at]  gmail  [dot]  com
-    <br>
+    <br><br> 
     [LinkedIn](https://www.linkedin.com/in/anikomaraz/) 
     <br>
     """, unsafe_allow_html=True)
